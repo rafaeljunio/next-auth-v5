1136f94e49c9ef7e1975e3a39cfb34c50c758666
Rafael Souza <rafaeljunio7@gmail.com>
2024-08-03 21:51:58 -0300

    refactor

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,16 +1,17 @@
 import { UserInfo } from "@/components/user-info";
+
 import { currentUser } from "@/lib/auth";
 
 const ServerPage = async () => {
 
+
+
+
+  
+  
   const user = await currentUser();
 
-  return (
-    <UserInfo 
-      label="Server component"
-      user={user}
-    />
-  );
+  return <UserInfo label="Server component" user={user} />;
 };
 
 export default ServerPage;
